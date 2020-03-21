e079cc1b363a24d2793eb4e357e83a30417bd812
Lin Naiyang <maxint@foxmail.com>
2020-03-21 21:50:49 +0800

    Add license

--- arcbuild.bat
+++ arcbuild.bat
@@ -1,3 +1,6 @@
 @echo off
+REM The MIT License (MIT)
+REM Copyright © 2016 Naiyang Lin <maxint@foxmail.com>
+
 set PATH=C:\Program Files\CMake\bin;C:\Program Files (x86)\CMake\bin;%PATH%
 cmake -P %~dp0arcbuild.cmake %*
--- arcbuild.sh
+++ arcbuild.sh
@@ -1,2 +1,5 @@
+# The MIT License (MIT)
+# Copyright © 2016 Naiyang Lin <maxint@foxmail.com>
+
 #!/usr/bin/env bash
 cmake -P $(dirname $0)/arcbuild.cmake $*
